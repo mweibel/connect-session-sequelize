d0799f3e0519cfda2d4ab3b551b93733e67b365d
Michael Weibel <michael.weibel@gmail.com>
2013-04-02 15:54:15 +0200

    Implemented connect-session-sequelize

---
+++ README.md
@@ -0,0 +1,31 @@
+# Connect Session Store using Sequelize
+[![Build Status](https://travis-ci.org/mweibel/connect-session-sequelize.png)](https://travis-ci.org/mweibel/connect-session-sequelize)
+
+connect-session-sequelize is a SQL session store using [Sequelize.js](http://sequelizejs.com).
+
+# Installation
+
+```
+$ npm install connect-session-sequelize
+```
+
+# Options
+
+* `db` a successfully connected Sequelize instance
+
+# Usage
+
+```javascript
+var connect = require('connect')
+	// for express, just call it with 'express'
+	, SequelizeStore = require('connect-session-sequelize')(connect);
+
+connect().use(connect.session({
+	store: new SequelizeStore(options)
+	, secret: 'CHANGEME'
+}));
+```
+
+# License
+
+MIT
---
+++ test.js
@@ -0,0 +1,40 @@
+
+var assert = require('assert')
+	, connect = require('connect')
+	, SequelizeStore = require('./lib/connect-session-sequelize')(connect)
+	, Sequelize = require('sequelize');
+
+var db = new Sequelize('session_test', 'test', '12345', {
+		dialect: 'sqlite'
+		, logging: false
+	})
+	, store = new SequelizeStore({db: db})
+	, sessionId = '1234a'
+	, sessionData = {foo: 'bar', 'baz': 42};
+
+describe('connect-session-middleware', function() {
+	it('should have no length', function() {
+		store.length(function(c) {
+			assert.equals(0, c);
+		});
+	});
+	it('should save the session', function() {
+		store.set(sessionId, sessionData, function(err, session) {
+			assert.ok(!err, '#set() got an error');
+			assert.ok(session, '#set() is not ok');
+
+			store.length(function(c) {
+				assert.equals(1, c);
+
+				store.get(sessionId, function(err, data) {
+					assert.ok(!err, '#get() got an error');
+					assert.deepEqual(sessionData, data);
+
+					store.destroy(sessionId, function(err) {
+						assert.ok(err, '#destroy() got an error');
+					});
+				});
+			});
+		});
+	});
+});
