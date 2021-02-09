33df841f4b1a2672a5df3b1aaa91bc4f97744def
Alan Morel <alan@alanmorel.com>
2021-02-09 18:36:39 -0500

    Add TypeScript declarations

--- index.d.ts
+++ index.d.ts
@@ -1,4 +1,4 @@
-import { Store } from 'express-session';
+import { SessionData, Store } from 'express-session';
 import { Sequelize, SyncOptions } from 'sequelize';
 
 interface DefaultFields {
@@ -23,6 +23,9 @@ declare class SequelizeStore extends Store {
   sync(options?: SyncOptions): void
   touch: (sid: string, data: any, callback?: (err: any) => void) => void
   stopExpiringSessions: () => void
+  get(sid: string, callback: (err: any, session?: SessionData | null) => void): void
+  set(sid: string, session: SessionData, callback?: (err?: any) => void): void
+  destroy(sid: string, callback?: (err?: any) => void): void
 }
 
 interface SequelizeStoreConstructor {
